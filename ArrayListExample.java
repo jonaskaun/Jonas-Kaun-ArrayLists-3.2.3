d0192a1e08298c48604357e623cdabfeed562464
jonaskaun <jonas.kaun@crpusd.org>
2026-02-10 17:57:25 -0800

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,26 @@
+/*
+ * Activity 3.2.3
+ */
+import java.util.ArrayList;
+
+public class ArrayListExample
+{  
+   public static void main(String[] args)
+   {  
+      ArrayList<String> animalList = new ArrayList<String>();  
+      animalList.add("Dog");
+      animalList.add("Cat");
+      animalList.add("Rabbit");
+      animalList.add("Frog");
+      animalList.add("Horse");
+      animalList.add("Cow");
+      
+      // display list
+      System.out.println(animalList);
+      
+      /* your code here */ 
+      
+      // display modified list
+      System.out.println(animalList);
+   }  
+}
