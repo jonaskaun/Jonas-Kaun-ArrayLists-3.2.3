7f6a00520a58cf128461e94d8c4da21c0a20e70d
Jonas <jonas.kaun@crpusd.org>
2026-02-10 18:57:59 -0800

    got up untill the actions of replace

---
+++ ArrayListMethods.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListMethods{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> rotCreature = new ArrayList<String>();
+        rotCreature.add("Tun Tun Tun Sahur");
+        rotCreature.add("Bombini Goosini");
+        rotCreature.add("Tralalero Tralala");
+        rotCreature.add("Bombardiro Crocodilo");
+        rotCreature.add("Cappuccino Assassino");
+        rotCreature.add("Chimpanzini Bananini");
+        int indexTemp;
+        int t = 0;
+        String input;
+        boolean quit = false;
+        while(quit == false){
+            System.out.println(rotCreature);
+            System.out.println("Would you like to (a)dd, (i)nsert, (r)emove, Re(p)lace or (q)uit?");
+            input = sc.nextLine();
+            if (input.equals("a")){
+                System.out.println("What would you like to add?");
+                input = sc.nextLine();
+                rotCreature.add(input);
+            }
+            else if (input.equals("i")){
+                System.out.println("\nWhat would you like to insert?");
+                input = sc.nextLine();
+                System.out.println("At what index would you like to insert it?");
+                indexTemp = sc.nextInt();
+                rotCreature.set(indexTemp, input);
+
+            }
+            else if (input.equals("r")){
+                System.out.println("\nReplace by (v)alue, or (i)ndex");
+                input = sc.nextLine();
+                if (input.equals("v")){
+                    
+                }
+                else if (input.equals("i")){
+
+                }
+                else{
+                    System.out.println("invalid input");
+                }
+
+            }
+            else if (input.equals("p")){
+                System.out.println("What value would you like to repace?");
+                input = sc.nextLine();
+                
+
+            }
+            else if (input.equals("q")){
+                quit = true;
+            }
+            else{
+                System.out.println("invalid input");
+            }
+
+
+            t++;
+        }
+        System.out.print("\nYour final list is:");
+        System.out.print("\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        System.out.print("\n"+rotCreature+"\n");
+        System.out.print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        System.out.print("\nThanks for playing! :)");
+
+
+        sc.close();
+    }
+    
+}
